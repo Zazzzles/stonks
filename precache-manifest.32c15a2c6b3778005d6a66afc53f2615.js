4982a9e1d52d4ba91054cbfb9fc82016e247625a
Ras Brits <rasbrits@gmail.com>
2022-06-24 16:07:11 +0200

    Updates

---
+++ precache-manifest.32c15a2c6b3778005d6a66afc53f2615.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee897d9ece79b592c6183673750f477",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9fa46eade22fcb922c9",
+    "url": "/static/css/main.d1a11958.chunk.css"
+  },
+  {
+    "revision": "29834449fc511a2d3af1",
+    "url": "/static/js/2.9f33eb80.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9f33eb80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fa46eade22fcb922c9",
+    "url": "/static/js/main.beb8c932.chunk.js"
+  },
+  {
+    "revision": "9629176c6229d3095cda",
+    "url": "/static/js/runtime-main.e07952ec.js"
+  },
+  {
+    "revision": "189c40dd96d807c7857cad09227088a7",
+    "url": "/static/media/Gilroy-Bold.189c40dd.ttf"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "3bb902bd62da53ba6e01923c36349b17",
+    "url": "/static/media/Gilroy-Medium.3bb902bd.ttf"
+  },
+  {
+    "revision": "c6dd5cb724d66fc4bfde3961466e1c16",
+    "url": "/static/media/Gilroy-Regular.c6dd5cb7.ttf"
+  }
+]);
